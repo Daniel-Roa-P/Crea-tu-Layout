05b40403380b2627adf9d698add10bcd4a8624a8
DanielRoa <danbron523@gmail.com>
2019-01-09 00:29:16 -0500

    añadi colores

--- app/src/main/java/com/example/danbr/nuevolayout/SegundoActivity.java
+++ app/src/main/java/com/example/danbr/nuevolayout/SegundoActivity.java
@@ -1,6 +1,8 @@
 package com.example.danbr.nuevolayout;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import com.bumptech.glide.Glide;
 public class SegundoActivity extends AppCompatActivity {
 
     public ImageView imagen;
+    public ConstraintLayout fondo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,14 @@ public class SegundoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_segundo);
 
         imagen = (ImageView) findViewById(R.id.imageView);
+        fondo = (ConstraintLayout) findViewById(R.id.fondo);
+
+        Resources resources = getResources();
+
+        int resId = resources.getIdentifier("Azul", "color", getPackageName());
+
         Glide.with(this).load(R.raw.gatosgif).into(imagen);
+        fondo.setBackgroundColor(getResources().getColor(resId));
     }
 
     public void cambiar(View view){
